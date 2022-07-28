9b561fa8bb5a84dd812766e3dc0c35e2c0c6f203
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-07-27 22:28:37 -0400

    fixed typos

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const thoughtsRoutes = require('./thoughtRoute.js');
-const userRoutes = require('./userRoute.js');
+const thoughtRoute = require('./thoughtRoute.js');
+const userRoute = require('./userRoute.js');
 
 router.use('/users', userRoute);
 router.use('/thoughts', thoughtRoute);
