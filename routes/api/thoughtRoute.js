139d12b0999f7345188843115223c006d6095f68
Garrett Lee <43485454+RGarrettLee@users.noreply.github.com>
2022-07-27 22:17:09 -0400

    created thought route

---
+++ routes/api/thoughtRoute.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction,
+} = require('../../controllers/thoughtsController');
+
+// thoughts endpoint
+router.route('/').get(getThoughts).post(createThought);
+
+// thoughtId endpoint
+router
+    .route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// reacitons endpoint
+router.route('/:thoughtId/reactions').post(addReaction);
+
+// reactionId endpoint
+router.route('/:thoughtId/reactions/:reactionId').delete(removeReaction);
+
+module.exports = router;
